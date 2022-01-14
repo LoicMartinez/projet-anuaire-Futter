f766f079f52d7ded7f15d52219dd9531cee5da5b
Loïc <loicmartinez3000@gmail.com>
2022-01-14 22:41:16 +0100

    data_source

---
+++ lib/models/data_source.dart
@@ -0,0 +1,51 @@
+import 'package:projetflutter/models/class_contact.dart';
+
+class DataSource {
+  static List<FeuilleContact> feuilleContactList = <FeuilleContact>[
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net"),
+    FeuilleContact(
+        firstName: "Jean-Paul",
+        lastName: "Larue",
+        personalPhoneNumber: "06 71 59 57 60",
+        mail: "jp.larue@laposte.net")
+  ];
+}
