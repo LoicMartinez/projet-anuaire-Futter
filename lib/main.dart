a60ad2deeb7dbf64447cd20f710a8f752d7b6900
FlorianNm <florian.nemoz@epsi.fr>
2022-01-14 13:51:12 +0100

    mise à jour page par défaut

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialRoute: "/contact_detail",
+      initialRoute: "/contact",
       getPages: [
         GetPage(
             name: "/contact",
